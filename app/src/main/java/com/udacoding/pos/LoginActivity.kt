d711fa4a6cd8803017b79a7a4b117ff58273cb82
Programmer <programmer@localhost.localdomain>
2021-10-02 16:13:52 +0700

    Update validation email

--- app/src/main/java/com/udacoding/pos/LoginActivity.kt
+++ app/src/main/java/com/udacoding/pos/LoginActivity.kt
@@ -1,11 +1,38 @@
 package com.udacoding.pos
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import com.udacoding.pos.databinding.ActivityLoginBinding
+import com.udacoding.pos.utils.emailPattern
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        validateEmail()
+
     }
+
+    private fun validateEmail() {
+        binding.inputEmail.doAfterTextChanged {
+            if (!it.toString().matches(emailPattern().toRegex())){
+                binding.textInputLayoutEmail.error = getString(R.string.message_email_invalid)
+            } else {
+                binding.textInputLayoutEmail.isErrorEnabled = false
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/udacoding/pos/utils/Constant.kt
@@ -0,0 +1,6 @@
+package com.udacoding.pos.utils
+
+
+fun emailPattern() : String {
+    return "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+}
